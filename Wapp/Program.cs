7588e3c90b13d636de6040882ffbbbabe3f82cb7
jeremiastomas72@gmail.com <90426798+JeremiasTO@users.noreply.github.com>
2021-12-29 23:13:06 -0500

    Add project files.

---
+++ Wapp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Wapp
+{
+    class Program
+    {
+        public static int secondsCount = 0;
+       public static Timer aTmr = new Timer(2000);
+        static void Main(string[] args)
+        {
+            aTmr.Elapsed += ATmr_Elapsed;
+            aTmr.Enabled = true;
+            aTmr.AutoReset = true;
+            aTmr.Start();
+            aTmr.Stop();
+            Console.ReadKey();
+        }
+        private static void ATmr_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            secondsCount++;
+            Console.WriteLine(secondsCount + "Seconds");
+        }
+    }
+}
